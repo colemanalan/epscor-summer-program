b27fb9706df1ee5f7c1f2a3cdadc65ac841e2ced
Alan Coleman <alanc@udel.edu>
2021-06-22 22:05:39 -0500

    Updated to include an array job example

--- examples/cluster/ExampleSubmission.sh
+++ examples/cluster/ExampleSubmission.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-echo "I got some arguments in $0"
-echo "The first argument is:  $1"
-echo "The second argument is:  $2"
+#This program shows how to use arguments
+echo "I got some arguments in $0" #  $0 always stores the name of the program
+echo "The first argument is:  $1" #  $1 is the first argument passed
+echo "The second argument is:  $2" # $2 is the second one, etc.
 
 
+#This loop will nap for the number of seconds that you passed in argument 2
 echo ""
 echo "I will nap based on your second argument"
 for i in $(seq 1 $2); do
@@ -14,6 +16,7 @@ done
 echo "Ok I am awake now"
 echo ""
 
+#Make an error so we can see it later
 echo "I am going to generate some error output"
 ls /this/does/not/exist.txt
 echo ""
--- examples/cluster/MyBashScript.sh
+++ examples/cluster/MyBashScript.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-YOUR_NAME=$(whoami)
+YOUR_NAME=$(whoami)   #Store your username into the var "YOUR_NAME"
 
 echo "Hello ${YOUR_NAME}! Welcome to the bash script"
 echo ""
 
-WHERE_AM_I=$(pwd)
+WHERE_AM_I=$(pwd)   #Store your current directory
 echo "You are here: $WHERE_AM_I"
 echo ""
 
 echo "I am going to take a nap now"
-for i in {1..5}; do
+for i in {1..5}; do  #Loop over the numbers 1 - 5 and sleep for 2 sec
   echo "...zzzzz ($i)"
   sleep 2
 done
 echo "Ok I am awake now"
 echo ""
 
-FILES_IN_HOME_DIR=$(ls ~/)
+FILES_IN_HOME_DIR=$(ls ~/)  #Store the names of all files in your home dir
 echo "Your home directory files are:"
 echo $FILES_IN_HOME_DIR
 echo ""
 
-if [[ -f ~/.bashrc ]]; then
+if [[ -f ~/.bashrc ]]; then  #Check to see if your bashrc exists
   echo "Oh good, it looks like you have a .bashrc, you will need that"
 else
   echo "Oh no! You don't have a .bashrc"
 fi
 echo ""
 
-if [[ -d ~/work ]]; then
+if [[ -d ~/work ]]; then  #Check to see if you have a directory called "work"
   echo "Oh good, you have a work directory"
 else
   echo "You don't have a work directory called ~/work"
